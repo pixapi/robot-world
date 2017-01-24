13c2c48b3340be06c3a2741b393d5da217db0bb2
Amara Lovato <amaracuenca@gmail.com>
2017-01-24 16:01:57 -0700

    first commit

--- app/controllers/robot_world_app.rb
+++ app/controllers/robot_world_app.rb
@@ -3,8 +3,13 @@ require_relative '../models/robot.rb'
 class RobotWorldApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
 
+  get '/' do
+    erb :main
+  end
+
+
   get '/robots' do
+    @robots = Robot.all
     erb :index
   end
 end
-
--- app/models/robot.rb
+++ app/models/robot.rb
@@ -1,8 +1,32 @@
 require 'sqlite3'
 
 class Robot
+  attr_reader :name, :city, :state, :department
   def initialize(robot_params)
+    @name       = robot_params["name"]
+    @city       = robot_params["city"]
+    @state      = robot_params["state"]
+    @department = robot_params["department"]
     @database = SQLite3::Database.new('db/robot_world_development.db')
     @database.results_as_hash = true
+    @id = robot_params["id"] if robot_params["id"]
+  end
+
+  def save
+    @database.execute("INSERT INTO tasks (name, city, state, department) VALUES (?, ?);", @name, @city, @state, @department)
+  end
+
+  def self.database
+    database = SQLite3::Database.new('db/robot_world_development.db')
+    database.results_as_hash = true
+    database
+  end
+
+  def self.all
+    database
+    robots = database.execute("SELECT * FROM robots")
+    robots.map do |robot|
+      Robot.new(robot)
+    end
   end
 end
